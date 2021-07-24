c57d45a904709cd49773610c5d0c268cb505b4ef
jang-jinyeol <84236806+jang-jinyeol@users.noreply.github.com>
2021-07-24 15:32:12 +0900

    Create 전화번호 목록.cpp

---
+++ 전화번호 목록.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+
+    sort(phone_book.begin(), phone_book.end());
+
+    for (int i = 0; i < phone_book.size() - 1; i++) {
+
+        if (phone_book[i] == phone_book[i + 1].substr(0, phone_book[i].size()))
+            return false;
+    }
+    return answer;
+}
+
+int main() {
+    vector<string> phone_book = { "119", "97674223", "1195524421" };
+
+ 
+    cout <<solution(phone_book);
+
+}
