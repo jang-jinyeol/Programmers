1efacd4c83ed654a8775fe6afda570d23280d460
jang-jinyeol <84236806+jang-jinyeol@users.noreply.github.com>
2021-07-28 21:05:04 +0900

    Create 기능개발.cpp

---
+++ 기능개발.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+  vector<int> answer;
+  int day;
+  int max_day = 0;
+
+  for (int i = 0; i < progresses.size(); ++i) {
+    day = (99 - progresses[i]) / speeds[i] + 1;
+    if (answer.empty() || max_day < day) {
+      answer.push_back(1); max_day = day;
+    } else answer.back()++;
+  } return answer;
+}
