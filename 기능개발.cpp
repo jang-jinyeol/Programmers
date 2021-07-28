d90703b031fe577f75a3145d997fbc3e57edbbe2
jang-jinyeol <84236806+jang-jinyeol@users.noreply.github.com>
2021-07-28 21:13:05 +0900

    Update 기능개발.cpp

--- 기능개발.cpp
+++ 기능개발.cpp
@@ -10,9 +10,9 @@ vector<int> solution(vector<int> progresses, vector<int> speeds) {
   int max_day = 0;
 
   for (int i = 0; i < progresses.size(); ++i) {
-    day = (99 - progresses[i]) / speeds[i] + 1;
-    if (answer.empty() || max_day < day) {
-      answer.push_back(1); max_day = day;
-    } else answer.back()++;
+    day = (99 - progresses[i]) / speeds[i] + 1;//작업 진도는 100 미만 자연수 진도-작업률 / 속도 +1 은 총 걸리는 기간
+    if (answer.empty() || max_day < day) { //현재 값 보다 걸리는 기간이 더 길면
+      answer.push_back(1); max_day = day; //완료되는 작업 추가
+    } else answer.back()++;//걸리는 기간이 더 짧을 경우 더해줌
   } return answer;
 }
